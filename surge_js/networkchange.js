dbf7ddc13eaa1cb6ff21038d75cc75d0954ca1b3
xiaojunlin <1508664516@qq.com>
2021-05-04 08:20:15 +0800

    surge js

--- surge_js/networkchange.js
+++ surge_js/networkchange.js
@@ -1,11 +1,11 @@
 var wifiname = $network.wifi.ssid;
 var ipname = $network.v4.primaryAddress;
-var proxyip = ["192.168.50.100", "192.168.50.96"];
+var proxyip = ["192.168.50.100"];
 var proxywifi = ["JTY_5G","JTY"];
 for (var i = 0; i < proxywifi.length; i++) {
 	if (wifiname==proxywifi[i]){
 		$surge.setOutboundMode("direct");		
-		setTimeout(function(){$notification.post("network-changed","您目前处于WiFi-Proxy"+"SSID: "+wifiname,"Surge 自动变为直接连接");}, 3000);
+		setTimeout(function(){$notification.post("network-changed","您目前处于Home Network: "+wifiname,"Surge 自动变为直接连接");}, 3000);
 		break;		
 	};
 	if (i==proxywifi.length-1){
@@ -17,7 +17,7 @@ for (var i = 0; i < proxywifi.length; i++) {
 for (var i = 0; i < proxyip.length; i++) {
 	if (ipname==proxyip[i]){
 		$surge.setOutboundMode("direct");		
-		setTimeout(function(){$notification.post("network-changed","您目前处于Proxy-IP: "+ipname,"Surge 自动变为直接连接");}, 3000);
+		setTimeout(function(){$notification.post("network-changed","您目前处于Home Network: "+ipname,"Surge 自动变为直接连接");}, 3000);
 		break;		
 	};
 	if (i==proxyip.length-1){
