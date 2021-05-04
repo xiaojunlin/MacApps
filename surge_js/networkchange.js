04d8d1abc6912cdc9e6eb19139a7411f74359b7a
xiaojunlin <1508664516@qq.com>
2021-05-04 14:15:30 +0800

    surge js

--- surge_js/networkchange.js
+++ surge_js/networkchange.js
@@ -5,22 +5,22 @@ var proxywifi = ["JTY_5G"];
 for (var i = 0; i < proxywifi.length; i++) {
 	if (wifiname==proxywifi[i]){
 		$surge.setOutboundMode("direct");		
-		setTimeout(function(){$notification.post("network-changed","您目前处于Home WIFI-Network: "+wifiname,"Surge自动变为直接连接");}, 3000);
+		setTimeout(function(){$notification.post("Connect to Network: "+wifiname,"Surge has changed to Direct Outbound");}, 3000);
 		break;
 	};
 	if (ipname==proxyip[i]){
 		$surge.setOutboundMode("direct");		
-		setTimeout(function(){$notification.post("network-changed","您目前处于Home Network: "+ipname,"Surge自动变为直接连接");}, 3000);
+		setTimeout(function(){$notification.post("Connect to Network: "+ipname,"Surge has changed to Direct Outbound");}, 3000);
 		break;
 	};
 	if (i==proxywifi.length-1){
 		$surge.setOutboundMode("rule");
-		setTimeout(function(){$notification.post("Wi-Fi SSID ","Surge 自动变为规则模式","");}, 3000);
+		setTimeout(function(){$notification.post("Connect to Network: "+wifiname,"Surge has changed to Rule Proxy","");}, 3000);
 		break;
 	};
 	if (i==proxyip.length-1){
 		$surge.setOutboundMode("rule");		
-		setTimeout(function(){$notification.post("Connect IP ","Surge 自动变为规则模式","");}, 3000);
+		setTimeout(function(){$notification.post("Connect to Network: "+ipname,"Surge has changed to Rule Proxy","");}, 3000);
 		break;	
 	}
 };
