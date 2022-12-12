94401a155bc25235f3b7f72c20826aa96cc4e24e
Xiaojun Lin <31861515+xiaojunlin@users.noreply.github.com>
2022-12-12 21:00:36 +0800

    Update networkchange.js

--- surge_js/networkchange.js
+++ surge_js/networkchange.js
@@ -3,12 +3,12 @@ var proxywifi = ["JTY-5G"];
 for (var i = 0; i < proxywifi.length; i++) {
 	if (wifiname==proxywifi[i]){
 		$surge.setOutboundMode("direct");		
-		setTimeout(function(){$notification.post("Welcome home","Change to Direct Outbound","");}, 3000);
+		setTimeout(function(){$notification.post("¯\_(ツ)_/¯","Direct Outbound","");}, 3000);
 		break;
 	};
 	if (i==proxywifi.length-1){
 		$surge.setOutboundMode("rule");
-		setTimeout(function(){$notification.post("On the way","Changed to Rule-based Proxy","");}, 3000);
+		setTimeout(function(){$notification.post("\(^o^)/~","Rule-based Proxy","");}, 3000);
 		break;
 	};
 };
